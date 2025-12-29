e2f6cdb9338dca42e492e8fec3454b4417745f19
Vladimir V <vladimir.vorobiev@gmail.com>
2025-12-29 18:42:58 -0500

    Manage OPenAI API key

---
+++ packages/server/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import type { Request, Response } from "express";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
