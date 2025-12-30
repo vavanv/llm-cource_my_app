68d065e93bba018334e7b8812f052d6a130ce94c
Vladimir V <vladimir.vorobiev@gmail.com>
2025-12-29 22:36:59 -0500

    setup shadcn/ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [message, setMessage] = useState<string>("");
@@ -10,7 +11,12 @@ function App() {
       .catch((err) => console.error("Error fetching message:", err));
   }, []);
 
-  return <p className="font-bold p-4 text-3xl">{message}</p>;
+  return (
+    <div className="p-4">
+      <p className="font-bold text-3xl">{message}</p>
+      <Button onClick={() => alert("Button clicked!")}>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
